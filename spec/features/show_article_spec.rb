9d13937cd4984e0905f3314f52db73ebc66f7eec
Stephan Yu <Stephan.yu@gmail.com>
2016-11-04 14:04:53 +0000

    Hide edit and delete links from non-signed in users and non-owners

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.feature 'Showing an Article' do 
   before do 
-    @user = User.create(email: 'user@email.com', password: 'password')
-    login_as(@user)
-    @article = Article.create(title: 'A test article', body: 'A test body', user: @user)
+    @adam = User.create(email: 'adam@email.com', password: 'password')
+    @eve = User.create(email: 'eve@email.com', password: 'password')
+    @article = Article.create(title: 'A test article', body: 'A test body', user: @adam)
   end
+
   scenario 'A user clicks on an article to show its details' do 
     visit root_path
 
@@ -14,5 +15,31 @@ RSpec.feature 'Showing an Article' do
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
     expect(current_path).to eq(article_path(@article))
+  end
+
+  scenario 'A Non-signed in user cannot see the Edit and Delete button' do 
+    visit root_path
+
+    click_link @article.title
+    expect(page).not_to have_link('Edit Article')
+    expect(page).not_to have_link('Delete Article')
+  end
+
+  scenario 'A Non-owner user cannot see the Edit and Delete button' do 
+    login_as(@eve)
+    visit root_path
+
+    click_link @article.title
+    expect(page).not_to have_link('Edit Article')
+    expect(page).not_to have_link('Delete Article')
+  end
+
+  scenario 'A Signed-in Owner sees the Edit and Delete button' do 
+    login_as(@adam)
+    visit root_path
+
+    click_link @article.title
+    expect(page).to have_link('Edit Article')
+    expect(page).to have_link('Delete Article')
   end
 end
