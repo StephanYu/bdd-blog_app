752e7fc8cb96aeb6bf6737287289f9a8b2441397
Stephan Yu <Stephan.yu@gmail.com>
2016-11-04 13:28:44 +0000

    Add restrictions to Create Article button on view layer for non-signed in user

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 RSpec.feature 'Listing Articles' do 
   before do 
     @user = User.create(email: 'user@email.com', password: 'password')
-    login_as(@user)
     @article1 = Article.create(title: 'The first article', body: 'The first article body', user: @user)
     @article2 = Article.create(title: 'The second article', body: 'The second article body', user: @user)
   end
 
-  scenario 'A user lists all articles' do 
+  scenario 'Lists all articles with a non-signed in user' do 
     visit root_path
 
     expect(page).to have_content(@article1.title) 
@@ -17,7 +16,20 @@ RSpec.feature 'Listing Articles' do
     expect(page).to have_content(@article2.body) 
     expect(page).to have_link(@article1.title) 
     expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link('Create Article')
+  end
 
+  scenario 'Lists all articles with a signed in user' do 
+    login_as(@user)
+    visit root_path
+
+    expect(page).to have_content(@article1.title) 
+    expect(page).to have_content(@article1.body) 
+    expect(page).to have_content(@article2.title) 
+    expect(page).to have_content(@article2.body) 
+    expect(page).to have_link(@article1.title) 
+    expect(page).to have_link(@article2.title)
+    expect(page).to have_link('Create Article')
   end
 
   scenario 'A user has no articles' do 
